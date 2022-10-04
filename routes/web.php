7707001d0c65082370e2bc025496f32bb55d38e5
Hnin Oo Wai Lwin <hninoowaiucsy@gmail.com>
2022-10-04 13:17:26 +0630

    admin_middleware

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,17 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
-	
-	Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function() {
-		Route::get('/', 'HomeController@index')->name('home');
-		Route::resource('user', 'UserController', ['except' => ['show']]);
-		Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
-		Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
-		Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
-	});
+    Route::group(['middleware' => 'admin'], function () {
+        Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+            Route::get('/', 'HomeController@index')->name('home');
+            Route::resource('user', 'UserController', ['except' => ['show']]);
+            Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+            Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+            Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+        });
+    });
 
-	Route::group(['namespace' => 'UI'], function() {
-		Route::get('/', 'UIController@index')->name('luckydraw');
-	});
+    Route::group(['namespace' => 'UI'], function () {
+        Route::get('/', 'UIController@index')->name('luckydraw');
+    });
 });
-
