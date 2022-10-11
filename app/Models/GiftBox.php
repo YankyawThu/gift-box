508e278087444c592f7b09df30ebe99af1fb20dd
Hnin Oo Wai Lwin <hninoowaiucsy@gmail.com>
2022-10-11 11:12:17 +0630

    box_category

--- app/Models/GiftBox.php
+++ app/Models/GiftBox.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class GiftBox extends Model
 {
+    use SoftDeletes;
+
     protected $guarded = [];
 }
