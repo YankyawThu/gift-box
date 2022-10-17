3c487b284d50c94e565d5d21b0c4fafd8c8f075e
Hnin Oo Wai Lwin <hninoowaiucsy@gmail.com>
2022-10-17 17:29:04 +0630

    calculate probability

--- app/Http/Controllers/UI/UIController.php
+++ app/Http/Controllers/UI/UIController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\UI;
 
 use App\Http\Controllers\Controller;
+use App\Services\UI\BoxService;
+use Illuminate\Support\Facades\Auth;
 
 class UIController extends Controller
 {
@@ -11,7 +13,7 @@ class UIController extends Controller
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(BoxService $boxService)
     {
         // $this->middleware('auth');
     }
@@ -23,6 +25,46 @@ class UIController extends Controller
      */
     public function index()
     {
-        return view('ui.home');
+        $data = $this->boxService->getAll();
+
+        return view('ui.home', compact('data'));
+    }
+
+    public function openBox($id, $times)
+    {
+        // dd($times);
+        $data = $this->boxService->getAll();
+
+        if (Auth::check()) {
+            $giftBox = $this->boxService->getItemsByBoxId($id);
+            $items = [];
+            $itemsQty = [];
+            foreach ($giftBox->giftItemBox as $giftItemBox) {
+                $itemsQty[] = $giftItemBox->giftItems->qty;
+                $items[] = $giftItemBox->giftItems;
+            }
+            $sum = array_sum($itemsQty);
+
+            foreach ($items as $item) {
+                $item->probality = ($sum % $item->qty) / 100;
+            }
+            dd($items);
+            $rand = (rand(0, 1000) / 1000) * $sum;
+            $partialSum = 0;
+            for ($i = 0; $i < count($itemsQty); ++$i) {
+                $partialSum += $itemsQty[$i];
+                echo "ran : $rand";
+                echo "partial sum : $partialSum \n";
+                if ($partialSum >= $rand) {
+                    dd($items[$i]);
+                    $win_value = $items[$i];
+                    break;
+                }
+            }
+
+            return view('ui.home', compact('data', 'win_value'));
+        } else {
+            return redirect()->route('login');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,6 @@ Route::group(['middleware' => 'auth'], function () {
     Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
     Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
 
-
     Route::group(['namespace' => 'UI'], function () {
         Route::get('ui', [UIController::class, 'index'])->name('luckydraw');
     });
