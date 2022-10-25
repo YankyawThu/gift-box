e48cdec1d461533d643fd599e7774fd22863cd17
yankyawthu <yankyawthudev97@gmail.com>
2022-10-25 16:46:30 +0630

    ui home page

--- resources/js/pages/layouts/Footer.vue
+++ resources/js/pages/layouts/Footer.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        footer
+    <div class="grid grid-cols-4 fixed w-full bg-white bottom-0">
+        <div class="text-lg p-3 text-center cursor-pointer"><i class="fas fa-home"></i></div>
+        <div class="text-lg p-3 text-center cursor-pointer"><i class="fas fa-recycle"></i></div>
+        <div class="text-lg p-3 text-center cursor-pointer"><i class="fas fa-print"></i></div>
+        <div class="text-lg p-3 text-center cursor-pointer"><i class="fas fa-user"></i></div>
     </div>
 </template>
 
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,10 +2,10 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link href="{{ asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />    <link type="text/css" rel="stylesheet" href="{{ asset('css/ui.css') }}">
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/tailwind.css') }}">
     <link type="text/css" rel="stylesheet" href="{{ asset('css/ui.css') }}">
-    <link type="text/css" rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
