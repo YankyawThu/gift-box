c99779e29024f3fca6efa7cb11f6ad28dacb2a5f
yankyawthu <yankyawthudev97@gmail.com>
2022-10-27 16:29:19 +0630

    add box detail

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -3,7 +3,7 @@
         <div>
             <div class="font-bold text-sm">All</div>
             <div class="flex flex-wrap justify-around">
-                <div v-for="(box,i) in boxes" :key="i" class="w-96 rounded-xl shadow-sm my-2">
+                <Link :href="'/box/'+box.id" v-for="(box,i) in boxes" :key="i" class="w-96 rounded-xl shadow-sm my-2" as="button">
                     <div class="flex justify-between py-2 bg-gradient-to-r from-blue-100 to-purple-100 px-3 rounded-t-xl">
                         <div>{{box.name}} {{box.id}}</div>
                         <div class="self-end text-yellow-500"><span class="font-bold">{{box.price}}</span><span class="text-xs"> coins/draw</span></div>
@@ -17,7 +17,7 @@
                         <div class="text-xs text-gray-400">A total of products</div>
                         <div></div>
                     </div>
-                </div>
+                </Link>
             </div>
         </div>
     </main-layout>
@@ -27,10 +27,12 @@
 
 import mainLayout from './layouts/Main.vue'
 import axios from 'axios'
+import {Link} from '@inertiajs/inertia-vue'
 
 export default {
     components: {
-        mainLayout
+        mainLayout,
+        Link
     },
     data() {
         return {
---
+++ resources/js/pages/modals/Rule.vue
@@ -0,0 +1,11 @@
+<template>
+     <div class="boxModal">
+        <div class="boxModal-content">
+            <span class="close-button"><img src="/image/ui/Cross.svg"></span>
+            <div class="overflow-auto max-h-96">
+                <div>Hello,123</div>
+                <div>Save the wish value and redeem the happy wish prizeSave the wish value and redeem the happy wish prizeSave the wish value and redeem the happy wish prizeSave the wish value and redeem the happy wish prizeSave the wish value and redeem the happy wish prizeSave the wish value and redeem the happy wish prizeSave the wish value and redeem the happy wish prize</div>
+            </div>
+        </div>
+    </div>
+</template>
