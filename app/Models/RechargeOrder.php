2a6dc30a575b57cde3ff1c464f74faa1c3701dde
Hnin Oo Wai Lwin <hninoowaiucsy@gmail.com>
2022-10-28 17:09:49 +0630

    recharge_order

--- app/Models/RechargeOrder.php
+++ app/Models/RechargeOrder.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\models;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class RechargeOrder extends Model
 {
-    //
+    use SoftDeletes;
+    protected $guarded = [];
 }
