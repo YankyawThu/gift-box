af434191e3a25419e66b177d18056589a8e1743e
Hnin Oo Wai Lwin <hninoowaiucsy@gmail.com>
2022-10-31 16:49:52 +0630

    recharge_order

--- app/Http/Requests/GiftItemBox/CreateRequest.php
+++ app/Http/Requests/GiftItemBox/CreateRequest.php
@@ -11,10 +11,10 @@ class CreateRequest extends FormRequest
      *
      * @return bool
      */
-    // public function authorize()
-    // {
-    //     return true;
-    // }
+    public function authorize()
+    {
+        return true;
+    }
 
     /**
      * Get the validation rules that apply to the request.
