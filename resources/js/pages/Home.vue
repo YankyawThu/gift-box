013022f9370fb24df70b410c6b25ee4c90bcc50a
yankyawthu <yankyawthudev97@gmail.com>
2022-11-01 17:23:07 +0630

    modify home page ui

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,20 +1,40 @@
 <template>
     <main-layout>
         <div>
-            <div class="font-bold text-sm">All</div>
-            <div class="flex flex-wrap justify-around">
-                <Link :href="'/box/'+box.id" v-for="(box,i) in boxes" :key="i" class="w-96 rounded-xl shadow-sm my-2" as="button">
-                    <div class="flex justify-between py-2 bg-gradient-to-r from-blue-100 to-purple-100 px-3 rounded-t-xl">
-                        <div>{{box.name}} {{box.id}}</div>
-                        <div class="self-end text-yellow-500"><span class="font-bold">{{box.price}}</span><span class="text-xs"> coins/draw</span></div>
+            <div class="flex justify-between mx-3 py-2">
+                <div class="flex flex-row">
+                    <div><img src="https://1.bp.blogspot.com/-VpNN3ROqFSw/WRC7TRlPYfI/AAAAAAAAi3k/8P1JqjBG4186-fVjrjW3v21RnF8fZFRHACLcB/s1600/18221988_435811773439708_638227643630576354_n.jpg" alt="" class="home_avatar"></div>
+                    <div class="ml-2 self-center">
+                        <div class="text-xs text-gray-400">Hello...</div>
+                        <div class="font-bold text-lg">Thinzar Wint Kyaw</div>
                     </div>
-                    <div class="px-3">
+                </div>
+                <div>
+                </div>
+            </div>
+            <div class="m-3">
+                <input type="text" class="home_search py-2 px-4 rounded-full w-full" placeholder="Search">
+            </div>
+            <div class="flex flex-wrap justify-between p-2 mb-3">
+                <div class="w-40 p-2 rounded-full bg-white m-1 fun_menu_tab">Tide Play</div>
+                <div class="w-40 p-2 rounded-full bg-white m-1 fun_menu_tab">Recharge</div>
+                <div class="w-40 p-2 rounded-full bg-white m-1 fun_menu_tab">Shipping</div>
+                <div class="w-40 p-2 rounded-full bg-white m-1 fun_menu_tab">Helps</div>
+            </div>
+            <div class="font-bold text-sm ml-3">Recommended</div>
+            <div class="flex flex-wrap justify-around">
+                <Link :href="'/box/'+box.id" v-for="(box,i) in boxes" :key="i" class="box_layout w-40 rounded-3xl shadow-sm my-2 px-3" as="button">
+                    <div class="flex py-2 rounded-t-xl">
+                        <div class="grow text-white text-left truncate">{{box.name}}<div class="text-xs text-gray-300">products</div></div>
+                        <div class="flex-none w-12 self-center text-right font-bold" style="color:#FFC042;">$ {{box.price}}</div>
+                    </div>
+                    <div class="">
                         <div v-for="(item,j) in box.items" :key="j" class="inline-block pr-1 py-3">
-                            <img :src="item.image" width="50" height="50">
+                            <img :src="item.image" width="35" height="35">
                         </div>
                     </div>
-                    <div class="flex justify-between py-2 px-3">
-                        <div class="text-xs text-gray-400">A total of products</div>
+                    <div class="flex justify-between py-2">
+                        <div style="color: #FFC042;"><span> coins/draw</span></div>
                         <div></div>
                     </div>
                 </Link>
--- resources/js/pages/layouts/Footer.vue
+++ resources/js/pages/layouts/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="grid grid-cols-4 fixed w-full bg-white bottom-0">
-        <div class="text-lg p-3 text-center cursor-pointer"><i class="fas fa-home"></i></div>
-        <div class="text-lg p-3 text-center cursor-pointer"><i class="fas fa-recycle"></i></div>
-        <div class="text-lg p-3 text-center cursor-pointer"><i class="fas fa-print"></i></div>
-        <div class="text-lg p-3 text-center cursor-pointer"><i class="fas fa-user"></i></div>
+    <div class="grid grid-cols-4 fixed w-full bg-white bottom-0 footer_nav text-gray-400">
+        <div class="text-lg p-3 text-center cursor-pointer"><i class="fas fa-home"></i><div class="text-sm">Home</div></div>
+        <div class="text-lg p-3 text-center cursor-pointer"><i class="fas fa-recycle"></i><div class="text-sm">Exchange</div></div>
+        <div class="text-lg p-3 text-center cursor-pointer"><i class="fas fa-print"></i><div class="text-sm">Wallet</div></div>
+        <div class="text-lg p-3 text-center cursor-pointer"><i class="fas fa-user"></i><div class="text-sm">Profile</div></div>
     </div>
 </template>
 
