6ff0664ccdd9c79ab7adde358f6468b7e1a8c8bf
Hnin Oo Wai Lwin <hninoowaiucsy@gmail.com>
2022-12-05 16:38:52 +0630

    fix

--- resources/views/admin/recharge_lists/index.blade.php
+++ resources/views/admin/recharge_lists/index.blade.php
@@ -52,7 +52,7 @@
                 @foreach ($data as $key => $item)
                 <tr>
                     <td>
-                        <p class="font-weight-bold mb-0 text-sm">{{ $loop->iteration }}</p>
+                        <p class="font-weight-bold mb-0 text-sm">{{ ++$key }}</p>
                     </td>
                     <td>
                         <p class="font-weight-bold mb-0 text-sm">{{ $item->amount }}</p>
